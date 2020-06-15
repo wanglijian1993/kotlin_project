2f29fb1fc6116fb79c1b0dc6b91c4810f1f21a8b
wanglijian <wanglijian1214@163.com>
2020-06-15 23:15:52 +0800

    1.学习lifecycle生命周期监听

--- app/src/main/java/com/android/frameworkkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/android/frameworkkotlin/activities/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.android.frameworkkotlin.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Debug
-import android.view.View
-import androidx.collection.ArraySet
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.android.frameworkkotlin.R
 import com.android.frameworkkotlin.adapter.ContentAdapter
@@ -18,28 +13,22 @@ import com.android.frameworkkotlin.fragments.VideoFragment
 import com.android.frameworkkotlin.utils.tabDatas
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.util.*
 
 /**
  * 首页MainActivity
  */
 class MainActivity : BaseActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
          setContentView(R.layout.activity_main)
-        var adapter = ContentAdapter(supportFragmentManager,initFragments())
-        vp_content.setAdapter(adapter)
-        vp_content.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL)
+        val adapter = ContentAdapter(supportFragmentManager,initFragments())
+        vp_content.adapter = adapter
+        vp_content.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         tabDatas.forEach{
             tl_bottom_control.addTab(tl_bottom_control.newTab().setText(it))
         }
         vp_content.registerOnPageChangeCallback(onPageChangeCallBack)
         tl_bottom_control.addOnTabSelectedListener(onTabSelectCallBack)
-
     }
 
 
@@ -49,22 +38,10 @@ class MainActivity : BaseActivity() {
          val videoFragment=VideoFragment.instance
         val mineFragment=MineFragment.instance
         val fragments = listOf(homefragment,informationFragment,videoFragment,mineFragment)
-        return fragments;
+        return fragments
     }
 
     val onPageChangeCallBack=object :ViewPager2.OnPageChangeCallback(){
-
-        override fun onPageScrollStateChanged(state: Int) {
-            super.onPageScrollStateChanged(state)
-        }
-
-        override fun onPageScrolled(
-            position: Int,
-            positionOffset: Float,
-            positionOffsetPixels: Int
-        ) {
-            super.onPageScrolled(position, positionOffset, positionOffsetPixels)
-        }
 
         override fun onPageSelected(position: Int) {
             super.onPageSelected(position)
@@ -73,17 +50,17 @@ class MainActivity : BaseActivity() {
     }
     val onTabSelectCallBack=object : TabLayout.OnTabSelectedListener{
         override fun onTabReselected(p0: TabLayout.Tab?) {
-         println("onTabReselected-----"+p0?.position);
+         println("onTabReselected-----"+p0?.position)
         }
 
         override fun onTabUnselected(p0: TabLayout.Tab?) {
-            println("onTabUnselected-----"+p0?.position);
+            println("onTabUnselected-----"+p0?.position)
 
         }
 
         override fun onTabSelected(p0: TabLayout.Tab?) {
-            println("onTabSelected-----"+p0?.position);
-            p0?.position?.let { vp_content.setCurrentItem(it) }
+            println("onTabSelected-----"+p0?.position)
+            p0?.position?.let { vp_content.currentItem = it }
 
         }
     }
