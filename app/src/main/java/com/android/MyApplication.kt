fa68763e890c7fdc5cb6127d9cff6db7562e5371
wanglijian <wanglijian1214@163.com>
2020-06-15 21:37:33 +0800

    网络okhttp替换Retrofit

---
+++ app/src/main/java/com/android/MyApplication.kt
@@ -0,0 +1,20 @@
+package com.android
+
+import android.app.Application
+import android.content.Context
+import kotlin.properties.Delegates
+
+/**
+ * 当前类的注释:application
+ * 作者：WangLiJian on 2020/6/15.
+ * 邮箱：wanglijian1214@gmail.com
+ */
+class MyApplication: Application() {
+    companion object{
+        var appContext:Context by Delegates.notNull()
+    }
+    override fun onCreate() {
+        super.onCreate()
+        appContext=this
+    }
+}
--- app/src/main/java/com/android/frameworkkotlin/network/MyRetrofit.kt
+++ app/src/main/java/com/android/frameworkkotlin/network/MyRetrofit.kt
@@ -1,9 +1,13 @@
 package com.android.frameworkkotlin.network
 
-import okhttp3.OkHttpClient
+import com.android.MyApplication
+import com.android.frameworkkotlin.utils.NetworkUtil
+import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
 
 /**
  * 当前类的注释:封装Retrofit
@@ -11,20 +15,116 @@ import retrofit2.converter.gson.GsonConverterFactory
  * 邮箱：wanglijian1214@gmail.com
  */
 
-class MyRetrofit {
+object MyRetrofit {
 
-    companion object{
-        //日志拦截器
-        var httpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        //Okhttp对象
-        var okHttpClient = OkHttpClient.Builder()
-            .addInterceptor(httpLoggingInterceptor)
-            .build()
-        val retrofit = Retrofit.Builder().baseUrl(baseurl)
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create()).build()
-        val api= retrofit.create(ApiServices::class.java)
+    val api:ApiServices by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+        getRetrofit().create(ApiServices::class.java)
     }
-    
 
+//    companion object{
+//        //日志拦截器
+//        var httpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+//        //Okhttp对象
+//        var okHttpClient = OkHttpClient.Builder()
+//            .addInterceptor(httpLoggingInterceptor)
+//            .build()
+//        val retrofit = Retrofit.Builder().baseUrl(baseurl)
+//            .client(okHttpClient)
+//            .addConverterFactory(GsonConverterFactory.create()).build()
+//        val api= retrofit.create(ApiServices::class.java)
+//    }
+
+    /**
+     *获取retrofit的实例
+     */
+    fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(baseurl)  //自己配置
+            .client(getOkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    fun getOkHttpClient():OkHttpClient{
+        //添加一个log拦截器,打印所有的log
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        //可以设置请求过滤的水平,body,basic,headers
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        //设置 请求的缓存的大小跟位置
+        val cacheFile = File(MyApplication.appContext.cacheDir, "cache")
+        val cache = Cache(cacheFile, 1024 * 1024 * 50) //50Mb 缓存的大小
+
+        return OkHttpClient.Builder()
+//            .addInterceptor(addQueryParameterInterceptor())  //参数添加
+//            .addInterceptor(addHeaderInterceptor()) // token过滤
+//              .addInterceptor(addCacheInterceptor())
+            .addInterceptor(httpLoggingInterceptor) //日志,所有的请求响应度看到
+            .cache(cache)  //添加缓存
+            .connectTimeout(60L, TimeUnit.SECONDS)
+            .readTimeout(60L, TimeUnit.SECONDS)
+            .writeTimeout(60L, TimeUnit.SECONDS)
+            .build()
+    }
+    /**
+     * 设置缓存
+     */
+    private fun addCacheInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            var request = chain.request()
+            if (!NetworkUtil.isNetworkAvailable(MyApplication.appContext)) {
+                request = request.newBuilder()
+                    .cacheControl(CacheControl.FORCE_CACHE)
+                    .build()
+            }
+            val response = chain.proceed(request)
+            if (NetworkUtil.isNetworkAvailable(MyApplication.appContext)) {
+                val maxAge = 0
+                // 有网络时 设置缓存超时时间0个小时 ,意思就是不读取缓存数据,只对get有用,post没有缓冲
+                response.newBuilder()
+                    .header("Cache-Control", "public, max-age=" + maxAge)
+                    .removeHeader("Retrofit")// 清除头信息，因为服务器如果不支持，会返回一些干扰信息，不清除下面无法生效
+                    .build()
+            } else {
+                // 无网络时，设置超时为4周  只对get有用,post没有缓冲
+                val maxStale = 60 * 60 * 24 * 28
+                response.newBuilder()
+                    .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                    .removeHeader("nyn")
+                    .build()
+            }
+            response
+        }
+    }
+
+    /**
+     * 设置公共参数
+     */
+    private fun addQueryParameterInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val originalRequest = chain.request()
+            val request: Request
+            val modifiedUrl = originalRequest.url().newBuilder()
+                // Provide your custom parameter here
+                .addQueryParameter("xxx", "xxxx")
+                .build()
+            request = originalRequest.newBuilder().url(modifiedUrl).build()
+            chain.proceed(request)
+        }
+    }
+
+    /**
+     * 设置头
+     */
+    private fun addHeaderInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val originalRequest = chain.request()
+            val requestBuilder = originalRequest.newBuilder()
+                // Provide your custom header here
+                .header("token", "")
+                .method(originalRequest.method(), originalRequest.body())
+            val request = requestBuilder.build()
+            chain.proceed(request)
+        }
+    }
 }
