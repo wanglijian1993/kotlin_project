7f0dac4fd4bb95bf398636e9eb0ae29a37a01568
wanglijian <wanglijian1214@163.com>
2020-06-23 23:15:08 +0800

    添加Material design风格底部导航框

--- app/src/main/java/com/android/frameworkkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/android/frameworkkotlin/activities/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.android.frameworkkotlin.activities
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
 import com.android.frameworkkotlin.R
-import com.android.frameworkkotlin.adapter.ContentAdapter
 import com.android.frameworkkotlin.base.BaseActivity
 import com.android.frameworkkotlin.fragments.HomeFragment
 import com.android.frameworkkotlin.fragments.InformationFragment
 import com.android.frameworkkotlin.fragments.MineFragment
 import com.android.frameworkkotlin.fragments.VideoFragment
-import com.android.frameworkkotlin.utils.tabDatas
-import com.google.android.material.tabs.TabLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
@@ -20,51 +16,33 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         setContentView(R.layout.activity_main)
-        val adapter = ContentAdapter(this,initFragments())
-        vp_content.adapter = adapter
-        vp_content.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-        tabDatas.forEach{
-            tl_bottom_control.addTab(tl_bottom_control.newTab().setText(it))
-        }
-        vp_content.registerOnPageChangeCallback(onPageChangeCallBack)
-        tl_bottom_control.addOnTabSelectedListener(onTabSelectCallBack)
+        setContentView(R.layout.activity_main)
+        initListner()
     }
 
+    fun initListner() {
+        bnvBottomMenu.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            onTabItemSelected(item.itemId)
+            true
+        })
 
-    fun initFragments(): List<Fragment> {
-        val homefragment = HomeFragment.instance
-        val informationFragment = InformationFragment.instance
-         val videoFragment=VideoFragment.instance
-        val mineFragment=MineFragment.instance
-        val fragments = listOf(homefragment,informationFragment,videoFragment,mineFragment)
-        return fragments
     }
 
-    val onPageChangeCallBack=object :ViewPager2.OnPageChangeCallback(){
-
-        override fun onPageSelected(position: Int) {
-            super.onPageSelected(position)
-            tl_bottom_control.getTabAt(position)?.select()
-        }
-    }
-    val onTabSelectCallBack=object : TabLayout.OnTabSelectedListener{
-        override fun onTabReselected(p0: TabLayout.Tab?) {
-         println("onTabReselected-----"+p0?.position)
-        }
-
-        override fun onTabUnselected(p0: TabLayout.Tab?) {
-            println("onTabUnselected-----"+p0?.position)
-
-        }
-
-        override fun onTabSelected(p0: TabLayout.Tab?) {
-            println("onTabSelected-----"+p0?.position)
-            p0?.position?.let { vp_content.currentItem = it
+    private fun onTabItemSelected(id: Int) {
+        when (id) {
+            R.id.menu_main_bottom_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.flContent, HomeFragment.instance).commit()
+            R.id.menu_main_bottom_information -> supportFragmentManager.beginTransaction()
+                .replace(R.id.flContent, InformationFragment.instance).commit()
+            R.id.menu_main_botton_video -> supportFragmentManager.beginTransaction()
+                .replace(R.id.flContent, VideoFragment.instance).commit()
+            R.id.menu_main_botton_mine -> supportFragmentManager.beginTransaction()
+                .replace(R.id.flContent, MineFragment.instance).commit()
+            else -> {
             }
-
         }
     }
+
 
 }
 
